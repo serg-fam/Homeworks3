cecb59249c238849cd014088675db9038be3a59b
serg-fam <sergifam@gmail.com>
2016-05-27 16:17:41 +0300

    create folder

--- Homework3p1/src/Folder.java
+++ Homework3p1/src/Folder.java
@@ -1,10 +1,20 @@
 import java.util.List;
+import java.io.File;
 
 /**
  * Created by Serg-fam on 27.05.2016 on 12:29.
  */
 public class Folder {
 
+    File folder = new File("/tmp/a/b");
+
+    public java.io.File getFolder() {
+        return folder;
+    }
+
+    public void setFolder(java.io.File folder) {
+        this.folder = folder;
+    }
 
     private List<File> File;
 
